e31bbb12a90ccfa09e913dc08c08a52c986951f0
Gwyn Jones <m@gwnjns.com>
2016-12-03 22:25:35 +0000

    Add Twig view component (via slim/twig-view)
    Introduces the Twig templating language via the 'slim/twig-view'
    component for Slim. This is added using Composer.
    
    Having done so:
    
    * a 'home' template is introduced (within a 'templates' directory)
    * the '/' route is updated to render this view passing the Page data

--- index.php
+++ index.php
@@ -9,11 +9,26 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App(["settings" => $config]);
 
-// Route registration
-$app->get('/', function (Request $request, Response $response) {
+// Create a Dependency Injection Container
+$container = $app->getContainer();
+
+// Register view component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('templates', ['cache' => false]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
+// Render Twig template in route
+$app->get('/', function ($request, $response, $args) {
     $page = new gtvj\Page();
-    $response->getBody()->write($page->name);
-    return $response;
-});
+    return $this->view->render($response, 'home.html', [
+        'name' => $page->name
+    ]);
+})->setName('home');
 
 $app->run();
